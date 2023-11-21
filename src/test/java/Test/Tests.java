5276961d6bae726ab23db51230996b00776c870a
DessertWarrior <gly310@gmail.com>
2023-11-21 00:03:50 -0800

    added and modified tests -added gitignore

--- src/test/java/Test/Tests.java
+++ src/test/java/Test/Tests.java
@@ -15,11 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class Tests {
 
